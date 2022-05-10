a59aacd72f05b3c577ca363cfb943c11f00666f7
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-05-09 23:06:25 -0300

    bootstrap files removed

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 // Dom Variables
-const $hamburgerButton = document.getElementById("hamburger-button");
-const $logo = document.getElementById("logo");
-const $menuNav = document.getElementById("mobile-menu-id");
-const $closeButton = document.getElementById("close-button");
-const $portfolio = document.getElementById("porfolio");
-const $about = document.getElementById("about");
-const $contact = document.getElementById("contact");
-
+const $hamburgerButton = document.getElementById('hamburger-button');
+const $logo = document.getElementById('logo');
+const $menuNav = document.getElementById('mobile-menu-id');
+const $closeButton = document.getElementById('close-button');
+const $portfolio = document.getElementById('porfolio');
+const $about = document.getElementById('about');
+const $contact = document.getElementById('contact');
 
 // Functions
 function hideMobileMenu() {
-  $hamburgerButton.classList.remove("menu-display-off");
-  $logo.classList.remove("menu-display-off");
-  $menuNav.classList.add("menu-display-off");
+	$hamburgerButton.classList.remove('menu-display-off');
+	$logo.classList.remove('menu-display-off');
+	$menuNav.classList.add('menu-display-off');
 }
 
 // Dom Manipulation
-$hamburgerButton.addEventListener("click", () => {
-  $hamburgerButton.classList.add("menu-display-off");
-  $logo.classList.add("menu-display-off");
-  $menuNav.classList.remove("menu-display-off");
+$hamburgerButton.addEventListener('click', () => {
+	$hamburgerButton.classList.add('menu-display-off');
+	$logo.classList.add('menu-display-off');
+	$menuNav.classList.remove('menu-display-off');
 });
 
-$closeButton.addEventListener("click", () => {
-  hideMobileMenu();
+$closeButton.addEventListener('click', () => {
+	hideMobileMenu();
 });
 
-$portfolio.addEventListener("click", () => {
-  hideMobileMenu();
+$portfolio.addEventListener('click', () => {
+	hideMobileMenu();
 });
 
-$about.addEventListener("click", () => {
-  hideMobileMenu();
+$about.addEventListener('click', () => {
+	hideMobileMenu();
 });
 
-$contact.addEventListener("click", () => {
-  hideMobileMenu();
+$contact.addEventListener('click', () => {
+	hideMobileMenu();
 });
